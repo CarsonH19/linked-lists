bced6c240323ff79943646c362bac08c6bae9ab6
Carson Harp <carsonharp.fullstack@gmail.com>
2024-02-12 12:40:07 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+// npm install webpack webpack-cli --save-dev
+// npx webpack --watch
+// npm install --save-dev style-loader css-loader
+// npm install --save-dev csv-loader xml-loader
+
+// GitHub Pages Hosting:
+// npm run gh-deploy <already have a gh-pages branch>
+// npm run gh-deploy-init <don't have a gh-pages branch>
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    devtool: 'source-map',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+    ],
+  },
+};
