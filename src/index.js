40ec3cf51160c342f505635306bddfda2bf04426
CarsonH19 <questline.carson@gmail.com>
2024-02-12 13:12:11 +0900

    feat: add imports, linked list data, and logs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './style.css';
+
+import { LinkedList } from './linkedListModule';
+
+const listOfNames = new LinkedList();
+
+listOfNames.append("Carson");
+listOfNames.append("Joohee");
+listOfNames.append("Jacob");
+listOfNames.append("Ashton");
+listOfNames.append("Sam");
+listOfNames.append("Katlyn");
+
+console.log(listOfNames.toString(" "));
+console.log(listOfNames.listSize());
+console.log(listOfNames.listHead());
+console.log(listOfNames.listTail());
+console.log(listOfNames.at("Joohee"));
+listOfNames.pop();
+console.log(listOfNames.toString(" "));
+console.log(listOfNames.contains("Sam"));
+console.log(listOfNames.find("Jacob"))
